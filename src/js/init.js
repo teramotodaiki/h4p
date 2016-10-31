e259957021f10f1aa4fea46189266426e668d385
teramotodaiki <i101323@gmail.com>
2016-10-31 21:13:32 +0900

    fix

--- src/js/init.js
+++ src/js/init.js
@@ -19,28 +19,21 @@ export default () => {
     const {
       env,
       palette,
-      shot,
     } = (elem => {
       if (!elem) return {};
       const exported = JSON.parse(elem.textContent);
       return {
         env: importEnv(exported.env),
         palette: exported.palette,
-        shot: Object.assign({}, exported.shot || {}, {
-          name: '', text: ''
-        }),
       };
     })(document.querySelector('x-exports' + elem.getAttribute('data-target') + '__exports'));
 
-    return Promise.all([
-      makeFromElements(scripts),
-      makeFromType('text/javascript', shot),
-    ])
-    .then(([files, shot]) => {
-      // An instance of h4p.Player
-      const player = new Player({ files, env, palette, shot });
-      player.start();
-      return player;
-    });
+    return makeFromElements(scripts)
+      .then(files => {
+        // An instance of h4p.Player
+        const player = new Player({ files, env, palette });
+        player.start();
+        return player;
+      });
   });
 }
