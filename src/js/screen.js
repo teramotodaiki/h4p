7367dd5edaf276d7e33738f32b183626e3dd02c7
teramotodaiki <i101323@gmail.com>
2016-11-02 22:15:15 +0900

    rm

--- src/js/screen.js
+++ src/js/screen.js
@@ -1,4 +1,3 @@
-import eventemitter2Js from 'raw!eventemitter2';
 import requireJs from 'raw!../lib/require';
 import fetchJs from 'raw!whatwg-fetch';
 import screenJs from 'raw!../lib/screen';
@@ -6,7 +5,6 @@ import screenJs from 'raw!../lib/screen';
 
 export default [
 
-  eventemitter2Js,
   requireJs,
   fetchJs,
   screenJs,
