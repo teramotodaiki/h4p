1c992627960ea455a0b0f3b0fe7b8c63fca1ee13
teramotodaiki <i101323@gmail.com>
2016-11-26 15:23:00 +0900

    UI

--- src/Monitor/AboutDialog.jsx
+++ src/Monitor/AboutDialog.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import Dialog from 'material-ui/Dialog';
 import Table, { TableBody, TableRow, TableRowColumn } from 'material-ui/Table';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 
 const getStyles = (props, context) => {
@@ -19,12 +21,28 @@ export default class AboutDialog extends Component {
     localization: PropTypes.object.isRequired,
   };
 
+  state = {
+    inputCoreVersion: null
+  };
+
+  handleCoreVersionInput = (event) => {
+    const inputCoreVersion = event.target.value;
+    this.setState({ inputCoreVersion });
+  };
+
+  handleChangeVersion = () => {
+    const { inputCoreVersion } = this.state;
+  };
+
   render() {
     const {
       updateEnv,
       onRequestClose,
       localization: { aboutDialog },
     } = this.props;
+    const {
+      inputCoreVersion,
+    } = this.state;
 
     const { left } = getStyles(this.props);
 
@@ -43,6 +61,23 @@ export default class AboutDialog extends Component {
                 </TableRowColumn>
                 <TableRowColumn>
                 {CORE_VERSION}
+                </TableRowColumn>
+              </TableRow>
+              <TableRow>
+                <TableRowColumn style={left}>
+                {aboutDialog.changeVersion}
+                </TableRowColumn>
+                <TableRowColumn>
+                  <TextField
+                    id="ver"
+                    defaultValue={CORE_VERSION}
+                    onChange={this.handleCoreVersionInput}
+                  />
+                  <FlatButton primary
+                    label={aboutDialog.change}
+                    disabled={!inputCoreVersion}
+                    onTouchTap={this.handleChangeVersion}
+                  />
                 </TableRowColumn>
               </TableRow>
             </TableBody>
