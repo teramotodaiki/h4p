d4e4a047453affe7f8ea2c17e10c51ad047b055f
teramotodaiki <i101323@gmail.com>
2016-11-30 08:43:40 +0900

    move file

--- src/Monitor/Monitor.jsx
+++ src/Monitor/Monitor.jsx
@@ -7,7 +7,7 @@ import NavigationRefreh from 'material-ui/svg-icons/navigation/refresh';
 import transitions from 'material-ui/styles/transitions';
 
 
-import composeEnv from '../js/composeEnv';
+import composeEnv from '../File/composeEnv';
 import template from '../html/screen';
 import fallbackTemplate from '../html/dangerScreen';
 import screenJs from '../../lib/screen';
