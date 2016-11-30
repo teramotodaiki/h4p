2d6b4a2660e81131dbc638b3a83e4b4cc60a9ed3
teramotodaiki <i101323@gmail.com>
2016-11-30 17:19:21 +0900

    Load with type

--- src/File/index.js
+++ src/File/index.js
@@ -3,3 +3,4 @@ export { default as ConfigFile } from './ConfigFile';
 export { default as SourceFile } from './SourceFile';
 export { default as separate } from './separate';
 export { default as validateType } from './validateType';
+export { default as makeFromFile } from './makeFromFile';
