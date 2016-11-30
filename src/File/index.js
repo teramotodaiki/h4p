a504cf599a44b153100f2c2fe3fc0a21e74d653f
teramotodaiki <i101323@gmail.com>
2016-11-30 17:18:24 +0900

    migrate separete

--- src/File/index.js
+++ src/File/index.js
@@ -1,3 +1,5 @@
 export { default as BinaryFile } from './BinaryFile';
 export { default as ConfigFile } from './ConfigFile';
 export { default as SourceFile } from './SourceFile';
+export { default as separate } from './separate';
+export { default as validateType } from './validateType';
