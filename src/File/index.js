42084ff190282abbc32038d05c9078cdeeaaecc2
teramotodaiki <i101323@gmail.com>
2016-12-02 11:34:28 +0900

    load

--- src/File/index.js
+++ src/File/index.js
@@ -4,3 +4,4 @@ export { default as SourceFile } from './SourceFile';
 export { default as separate } from './separate';
 export { default as validateType } from './validateType';
 export { default as makeFromFile } from './makeFromFile';
+export { default as makeFromElements } from './makeFromElements';
