0585bc6b65c205baef2d136eddd183469970ccf9
teramotodaiki <i101323@gmail.com>
2016-12-07 11:20:58 +0900

    Use anyword hint

--- src/EditorPane/codemirror-hint-extension.js
+++ src/EditorPane/codemirror-hint-extension.js
@@ -1,30 +1,21 @@
 import CodeMirror from 'codemirror';
-import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/hint/anyword-hint';
 
+const anywordHint = CodeMirror.hint.anyword;
 
-const jsHint = CodeMirror.hint.javascript;
 CodeMirror.hint.javascript = (instance, options) => {
+
   const cursor = instance.getCursor();
   const token = instance.getTokenAt(cursor);
   const from = { line: cursor.line, ch: token.start };
   const to = { line: cursor.line, ch: cursor.ch };
+  const empty = { list: [], from, to };
 
-  if (endOfCompletion.test(token.string)) {
-    return { list: [], from, to };
+  if (/[\;\=\)\,]$|^\s*$|^\($/.test(token.string)) {
+    return empty;
   }
 
-  if (token.type === null && token.string !== '(') {
-    return { list: [], from, to };
-  }
-
-  options = Object.assign({}, options, jsOptions);
-
-  const result = jsHint(instance, options) || { list: [], from, to };
-
-  const snippets = (options.snippets['.source.js'] || [])
-    .filter((snippet) => snippet.test(token.string));
-
-  result.list = snippets.concat(result.list);
+  const result = anywordHint(instance, options) || empty;
 
   if (token.type === 'string') {
     const left = instance.getLine(cursor.line)
@@ -40,31 +31,5 @@ CodeMirror.hint.javascript = (instance, options) => {
   }
 
   return result;
+
 };
-
-const globalScope = (keys => {
-  const merge = (p, c) => {
-    if (typeof c === 'string') {
-      p[c] = null;
-      return p;
-    } else if (c instanceof Array) {
-      const key = c[0];
-      const props = c.slice(1);
-      p[key] = props.reduce(merge, Object.create(null));
-    }
-    return p;
-  };
-  return keys.reduce(merge, Object.create(null));
-})([
-  'require',
-  'exports',
-  ['module', 'exports'],
-  ['console', 'log', 'info', 'warn', 'error', 'time', 'timeEnd'],
-  ['env', 'DEBUG', 'VIEW']
-]);
-
-const jsOptions = {
-  globalScope
-};
-
-const endOfCompletion = /^(.*[\;\=\)\,]|\s*)$/;
