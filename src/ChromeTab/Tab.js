70e6352fc4ddfba8b152fe2b65ff5018a8c80080
teramotodaiki <i101323@gmail.com>
2016-12-11 13:42:34 +0900

    Tab state

--- src/ChromeTab/Tab.js
+++ src/ChromeTab/Tab.js
@@ -8,7 +8,7 @@ const getUniqueId = ((id) => () => 'Tab__' + ++id)(0);
 export default class Tab {
   constructor(props) {
     this.props = Object.freeze(props);
-    this.key = getUniqueId();
+    this.key = props.key || getUniqueId();
   }
 
   get file() {
@@ -19,13 +19,25 @@ export default class Tab {
     return this.props.component || this.file.component;
   }
 
+  get isSelected() {
+    return !!this.props.isSelected;
+  }
+
   is(tab) {
     return tab.file === this.file && tab.component === this.component;
   }
 
+  select(isSelected) {
+    const props = Object.assign({}, this.props, {
+      key: this.key,
+      isSelected,
+    });
+    return new Tab(props);
+  }
+
   renderContent(props) {
     return (
-      <ChromeTabContent key={this.key} show={props.isSelected}>
+      <ChromeTabContent key={this.key} show={this.isSelected}>
         <this.component file={this.file} {...props} />
       </ChromeTabContent>
     );
