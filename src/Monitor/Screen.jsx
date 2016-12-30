3ff23e3cad035050c163f2c3d2e4171ff8a21efd
teramotodaiki <i101323@gmail.com>
2016-12-30 16:05:46 +0900

    move right

--- src/Monitor/Screen.jsx
+++ src/Monitor/Screen.jsx
@@ -73,7 +73,7 @@ export default class Screen extends Component {
 
     const buttonStyle = {
       position: 'absolute',
-      left: 0,
+      right: 0,
       top: 0,
       backgroundColor: transparent,
       border: 'none',
