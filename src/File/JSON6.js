933811c26189e747c388a7d66d14aa24b1aeaa66
teramotodaiki <i101323@gmail.com>
2017-01-03 13:04:21 +0900

    JSON and template string

---
+++ src/File/JSON6.js
@@ -0,0 +1,20 @@
+// JSON and ES6 Template Literals
+
+export function parse(text) {
+
+  const escapeTL = (text) =>
+    text
+      .replace(/^\n/, '')
+      .replace(/\n/g, '\\n')
+      .replace(/\t/g, '\\t')
+      .replace(/\'/g, '\'')
+      .replace(/\"/g, '\"');
+
+  text =
+    text.split('`')
+    .map((seg, i) => i % 2 === 1 ? escapeTL(seg) : seg)
+    .join('\"');
+
+  return JSON.parse(text);
+
+};
